f70d7f63e2043febe143f5222bd68902736c08d7
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-06-17 16:05:06 -0700

    add pre-commit config and precommit hook changes
    use python:3.9 for base img to use baked in dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:3.9-slim-buster AS build
+FROM python:3.9 AS build
 ARG TFLINT_VERSION=0.23.0
 ARG TFSEC_VERSION=0.36.11
-ARG TFDOCS_VERSION=0.10.1
+ARG TFDOCS_VERSION=0.16.0
 ARG GIT_CHGLOG_VERSION=0.14.2
 ARG SEMTAG_VERSION=0.1.1
 ARG GH_VERSION=2.2.0
@@ -19,37 +19,37 @@ COPY install.sh /tmp/install.sh
 COPY requirements.txt /tmp/requirements.txt
 
 RUN chmod u+x /tmp/install.sh \
-    && sh /tmp/install.sh
+    && bash /tmp/install.sh
 
 FROM python:3.9-slim-buster
 ARG TERRAFORM_VERSION=latest
 ARG TERRAGRUNT_VERSION=latest
 
-# sets default shell to /bin/bash so `source` cmd is available
-SHELL ["/bin/bash", "-c"]
 WORKDIR /src/
 
 ENV VIRTUAL_ENV=/opt/base-venv
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 ENV PIP_DISABLE_PIP_VERSION_CHECK=1
-ENV PATH="/usr/local/.tfenv/bin:$PATH"
-# uses virtual env instead of system-wide packages
+ENV PATH="$VIRTUAL_ENV/.tfenv/bin:$PATH"
+# places virtual env in front of system-wide packages
 ENV PATH="$VIRTUAL_ENV/bin:$VIRTUAL_ENV/lib/python3.9/site-packages:$PATH"
 
-COPY --from=build /usr/local /usr/local
 COPY --from=build $VIRTUAL_ENV $VIRTUAL_ENV
 
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+# hadolint ignore=DL3008
 RUN apt-get -y update \
-    && apt-get install -y nodejs lbzip2 \
-    && apt-get install -y --no-install-recommends bash git curl unzip jq \
+    && apt-get install -y --no-install-recommends git nodejs lbzip2 jq \
     && ln -sf python3 /usr/local/bin/python \
     && git config --global advice.detachedHead false \
-    && tfenv install ${TERRAFORM_VERSION} \
-    && tfenv use ${TERRAFORM_VERSION} \
+    && tfenv install "${TERRAFORM_VERSION}" \
+    && tfenv use "${TERRAFORM_VERSION}" \
     && if [[ "$TERRAGRUNT_VERSION" == "latest" ]]; then \
         curl -s https://warrensbox.github.io/terragunt-versions-list/ | jq -r '.Versions[0]' | xargs -I {} tgswitch {}; \
-        else tgswitch "$TERRAGRUNT_VERSION"; fi
+    else tgswitch "$TERRAGRUNT_VERSION"; fi \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 
 COPY entrypoint.sh /tmp/entrypoint.sh
 
--- install.sh
+++ install.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
-apt-get -y update
-apt-get install -y aptitude
-aptitude install -y software-properties-common build-essential wget unzip git
+# shellcheck disable=SC1091
 
-python -m venv $VIRTUAL_ENV
-source $VIRTUAL_ENV/bin/activate
+set -e
+
+python -m venv "$VIRTUAL_ENV"
+source "$VIRTUAL_ENV"/bin/activate
+
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pip --upgrade --no-cache-dir -r /tmp/requirements.txt
 
-wget -q -O /tmp/tflint.zip https://github.com/terraform-linters/tflint/releases/download/v${TFLINT_VERSION}/tflint_linux_amd64.zip
+wget -q -O /tmp/tflint.zip https://github.com/terraform-linters/tflint/releases/download/v"${TFLINT_VERSION}"/tflint_linux_amd64.zip
 unzip -q /tmp/tflint.zip
-mv $(unzip -qql /tmp/tflint.zip | head -n1 | tr -s ' ' | cut -d' ' -f5-) /usr/local/bin/
+mv "$(unzip -qql /tmp/tflint.zip | head -n1 | tr -s ' ' | cut -d' ' -f5-)" "$VIRTUAL_ENV"/bin/
 
-wget -q -O /usr/local/bin/tfsec https://github.com/tfsec/tfsec/releases/download/v${TFSEC_VERSION}/tfsec-linux-amd64
-wget -q -O /usr/local/bin/terraform-docs https://github.com/terraform-docs/terraform-docs/releases/download/v${TFDOCS_VERSION}/terraform-docs-v${TFDOCS_VERSION}-linux-amd64
+wget -q -O "$VIRTUAL_ENV"/bin/tfsec https://github.com/tfsec/tfsec/releases/download/v"${TFSEC_VERSION}"/tfsec-linux-amd64
 
-wget -q -O /tmp/git-chglog.tar.gz https://github.com/git-chglog/git-chglog/releases/download/v${GIT_CHGLOG_VERSION}/git-chglog_${GIT_CHGLOG_VERSION}_linux_amd64.tar.gz
+wget -q -O /tmp/terraform-docs.tar.gz https://github.com/terraform-docs/terraform-docs/releases/download/v"${TFDOCS_VERSION}"/terraform-docs-v"${TFDOCS_VERSION}"-linux-amd64.tar.gz
+tar -zxf /tmp/terraform-docs.tar.gz -C /tmp
+mv /tmp/terraform-docs "$VIRTUAL_ENV"/bin/
+
+wget -q -O /tmp/git-chglog.tar.gz https://github.com/git-chglog/git-chglog/releases/download/v"${GIT_CHGLOG_VERSION}"/git-chglog_"${GIT_CHGLOG_VERSION}"_linux_amd64.tar.gz
 tar -zxf /tmp/git-chglog.tar.gz -C /tmp
-mv /tmp/git-chglog /usr/local/bin/
+mv /tmp/git-chglog "$VIRTUAL_ENV"/bin/
 
-wget -q -O /tmp/semtag.tar.gz https://github.com/nico2sh/semtag/archive/refs/tags/v${SEMTAG_VERSION}.tar.gz
+wget -q -O /tmp/semtag.tar.gz https://github.com/nico2sh/semtag/archive/refs/tags/v"${SEMTAG_VERSION}".tar.gz
 tar -zxf /tmp/semtag.tar.gz -C /tmp
-mv /tmp/semtag-${SEMTAG_VERSION}/semtag /usr/local/bin/
+mv /tmp/semtag-"${SEMTAG_VERSION}"/semtag "$VIRTUAL_ENV"/bin/
 
-wget -q -O /tmp/gh.tar.gz https://github.com/cli/cli/releases/download/v${GH_VERSION}/gh_${GH_VERSION}_linux_amd64.tar.gz
+wget -q -O /tmp/gh.tar.gz https://github.com/cli/cli/releases/download/v"${GH_VERSION}"/gh_"${GH_VERSION}"_linux_amd64.tar.gz
 tar -zxf /tmp/gh.tar.gz -C /tmp
-mv /tmp/gh_${GH_VERSION}_linux_amd64/bin/gh /usr/local/bin/
+mv /tmp/gh_"${GH_VERSION}"_linux_amd64/bin/gh "$VIRTUAL_ENV"/bin/
 
-wget -q -O /tmp/tfenv.tar.gz https://github.com/tfutils/tfenv/archive/refs/tags/v${TFENV_VERSION}.tar.gz
+wget -q -O /tmp/tfenv.tar.gz https://github.com/tfutils/tfenv/archive/refs/tags/v"${TFENV_VERSION}".tar.gz
 tar -zxf /tmp/tfenv.tar.gz -C /tmp
-mkdir /usr/local/.tfenv && mv /tmp/tfenv-${TFENV_VERSION}/* /usr/local/.tfenv && chmod u+x /usr/local/.tfenv/bin/tfenv
+mkdir "$VIRTUAL_ENV"/.tfenv && mv /tmp/tfenv-"${TFENV_VERSION}"/* "$VIRTUAL_ENV"/.tfenv && chmod u+x "$VIRTUAL_ENV"/.tfenv/bin/tfenv
 
-wget -q -O - https://raw.githubusercontent.com/warrensbox/tgswitch/release/install.sh | bash -s -- -b /usr/local/bin -d "${TGSWITCH_VERSION}"
-
-chmod u+x /usr/local/bin/*
-apt-get clean
-rm -rf /var/lib/apt/lists/*
-rm -rf /tmp/*
+wget -q -O - https://raw.githubusercontent.com/warrensbox/tgswitch/release/install.sh | bash -s -- -b "$VIRTUAL_ENV"/bin -d "${TGSWITCH_VERSION}"
+chmod u+x "$VIRTUAL_ENV"/bin/*
